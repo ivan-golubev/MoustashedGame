3feb57397722e6a77ea084d25baa0844b6ddfd0a
Ivan Golubev <ivan.golubev.spb@gmail.com>
2018-10-13 14:47:18 +0100

    Draggable cards & card mass
    Draggable cards & card mass

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    void OnMouseDrag()
+    {
+        float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+
+    }
+}
