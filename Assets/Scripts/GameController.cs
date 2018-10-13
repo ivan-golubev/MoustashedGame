690bf11bea82f0ed476a65f4c0e69eea89ad5f26
Ivan Golubev <ivan.golubev.spb@gmail.com>
2018-10-13 14:39:35 +0100

    Game Controller
    + Game Controller
    + spawning cards for 4 players

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField] private Transform[] playerLocations;
+    [SerializeField] private Player playerPrefab;
+    [SerializeField] private Transform playersRoot;
+
+    private Dictionary<int, Player> players = new Dictionary<int, Player>();
+    
+    void Start () {
+        foreach (var t in playerLocations) {
+            var player = Instantiate(playerPrefab);
+            player.transform.SetParent(playersRoot);
+            player.transform.position = t.position;
+            player.transform.localRotation = t.localRotation;
+            players[player.GetId()] = player;
+        }
+	}
+	
+}
